6de56122bb21d2937374841f9ca56762a9351c40
hiteshkataria <hitesh.kataria@idemia.com>
2018-11-25 10:30:15 +0530

    Understanding Bean LifeCyle

---
+++ src/main/java/com/practiceSpring/myspringapp/BeanLIfeCycleDemo.java
@@ -0,0 +1,36 @@
+package com.practiceSpring.myspringapp;
+
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.*;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+
+public class BeanLIfeCycleDemo implements BeanFactoryAware, ApplicationContextAware,BeanNameAware, DisposableBean,InitializingBean{
+
+    @Override
+    public void setBeanFactory(BeanFactory beanFactory) throws BeansException {
+        System.out.println("## Bean Factory has been set");
+    }
+
+    @Override
+    public void setBeanName(String s) {
+        System.out.println("## Bean Name has been set");
+    }
+
+    @Override
+    public void destroy() throws Exception {
+        System.out.println("## Bean is destroyed");
+    }
+
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        System.out.println("## After properties has been set");
+    }
+
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+        System.out.println("Application Context has been set"
+        );
+    }
+
+}
